26d2136e033d0240ba363accc1984ca0e220bf84
Arjun Sarao <arjunsarao727@gmail.com>
2025-10-26 13:56:45 -0400

    streak

---
+++ src/043_implement_ridge_regression_loss_function.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def ridge_loss(X: np.ndarray, w: np.ndarray, y_true: np.ndarray, alpha: float) -> float:
+    n = X.shape[0]
+    y_pred = X @ w
+    mse = 1.0 / n * np.sum((y_pred - y_true) ** 2)
+    regularization_term = alpha * np.sum(w**2)
+
+    return round(mse + regularization_term, 3)
+
+
+print(
+    ridge_loss(
+        X=np.array([[1, 1], [2, 1], [3, 1], [4, 1]]),
+        w=np.array([0.2, 2]),
+        y_true=np.array([2, 3, 4, 5]),
+        alpha=0.1,
+    )
+)
