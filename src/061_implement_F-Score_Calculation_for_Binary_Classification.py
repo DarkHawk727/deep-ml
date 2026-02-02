5bb03403cb81f91f3a38bbcaefe622d89635764b
Arjun Sarao <arjunsarao727@gmail.com>
2026-02-02 10:19:43 -0500

    F-Score question, filled in pyproject.toml and uv.lock

---
+++ src/061_implement_F-Score_Calculation_for_Binary_Classification.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def f_score(y_true: np.ndarray, y_pred: np.ndarray, beta: float) -> float:
+    tp: int = int(np.count_nonzero(y_true & y_pred))
+    fp: int = int(np.count_nonzero((~y_true) & y_pred))
+    fn: int = int(np.count_nonzero(y_true & (~y_pred)))
+
+    precision: float = tp / (tp + fp)
+    recall: float = tp / (tp + fn)
+
+    return round(
+        (1.0 + beta**2) * (precision * recall) / ((beta**2 * precision) + recall), 3
+    )
+
+
+print(f_score(np.array([1, 0, 1, 1, 0, 1]), np.array([1, 0, 1, 0, 0, 1]), 1.0))
